e3ed25361a5fc568ee938b857d95a1462071bd13
Moysin94 <shekhmoysin786@gmail.com>
2025-05-18 20:11:24 +0530

    added respones header verification steps

--- cypress/e2e/API Autoamtion Tests/createUser.cy.js
+++ cypress/e2e/API Autoamtion Tests/createUser.cy.js
@@ -1,8 +1,8 @@
 describe('post user suit', () => {
-    let accessToken='289ab6a617678f3417aa82b3c3a643f5816eac35594106545af2e8f4d70fac6a'
+    let accessToken = '289ab6a617678f3417aa82b3c3a643f5816eac35594106545af2e8f4d70fac6a'
     const randomNum = Math.floor(Math.random() * 100000);
     const email = `automation${randomNum}@gmail.com`;
-    const name=`automation${randomNum}`;
+    const name = `automation${randomNum}`;
     let userId;
     it('post user test1', () => {
         cy.request({
@@ -18,27 +18,32 @@ describe('post user suit', () => {
                 "status": "active"
             }
         }).then((resp) => {
-            userId= resp.body.data.id;
+            userId = resp.body.data.id;
             cy.log(JSON.stringify(resp));
             expect(resp.status).to.eq(201);
             expect(resp.body.data).have.property('id');
-            expect(resp.body.data).has.property('name',name);
-            expect(resp.body.data).has.property('email',email);
-            expect(resp.body.data).has.property('gender','male');
-            expect(resp.body.data).has.property('status','active');
-        }).then((resp)=>{
+            expect(resp.body.data).has.property('name', name);
+            expect(resp.body.data).has.property('email', email);
+            expect(resp.body.data).has.property('gender', 'male');
+            expect(resp.body.data).has.property('status', 'active');
+        }).then((resp) => {
             cy.request({
+                failOnStatusCode: false,
                 method: 'GET',
-                url: 'https://gorest.co.in/public/v2/users/'+userId,
-                headers:{
+                url: 'https://gorest.co.in/public/v2/users/' + userId,
+                headers: {
                     authorization: 'Bearer ' + accessToken
                 }
             })
-        }).then((resp)=>{
-             cy.log(JSON.stringify(resp));
-             expect(resp.status).to.eq(200);
-             expect(resp.body).have.property('id', userId);
+        }).then((resp) => {
+            cy.log(JSON.stringify(resp));
+            expect(resp.status).to.eq(200);
+            expect(resp.body).have.property('id', userId);
+            expect(resp.headers).to.include({
+                'content-type': 'application/json; charset=utf-8',
+                'connection': 'keep-alive'
+            });
+            expect(resp.headers).have.property('content-type','application/json; charset=utf-8')
         })
-
     })
 })
