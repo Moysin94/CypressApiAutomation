1190bdfbcd57d8b022f42f385a7bf9b792515295
Moysin94 <shekhmoysin786@gmail.com>
2025-05-18 19:43:13 +0530

    update user test added

---
+++ cypress/e2e/API Autoamtion Tests/updateUser.cy.js
@@ -0,0 +1,54 @@
+describe('post user suit', () => {
+    let accessToken='289ab6a617678f3417aa82b3c3a643f5816eac35594106545af2e8f4d70fac6a'
+    const randomNum = Math.floor(Math.random() * 100000);
+    const email = `automation${randomNum}@gmail.com`;
+    const name=`automation${randomNum}`;
+    const email2 = `automation${randomNum}@gmail.com`;
+    const name2=`automation${randomNum}`;
+    let userId;
+    it('post user test1', () => {
+        cy.request({
+            method: 'POST',
+            url: 'https://gorest.co.in/public/v1/users',
+            headers: {
+                authorization: 'Bearer ' + accessToken
+            },
+            body: {
+                "name": name,
+                "gender": "male",
+                "email": email,
+                "status": "active"
+            }
+        }).then((resp) => {
+            userId= resp.body.data.id;
+            cy.log(JSON.stringify(resp));
+            expect(resp.status).to.eq(201);
+            expect(resp.body.data).have.property('id');
+            expect(resp.body.data).has.property('name',name);
+            expect(resp.body.data).has.property('email',email);
+            expect(resp.body.data).has.property('gender','male');
+            expect(resp.body.data).has.property('status','active');
+        }).then((resp)=>{
+            cy.request({
+                method: 'PUT',
+                url: 'https://gorest.co.in/public/v2/users/'+userId,
+                headers:{
+                    authorization: 'Bearer ' + accessToken
+                },
+                body: {
+                "name": name2,
+                "gender": "male",
+                "email": email2,
+                "status": "active"
+            }
+            })
+        }).then((resp)=>{
+             cy.log(JSON.stringify(resp));
+             expect(resp.status).to.eq(200);
+             expect(resp.body).have.property('id', userId);
+             expect(resp.body).has.property('name',name2);
+             expect(resp.body).has.property('email',email2);
+        })
+
+    })
+})
